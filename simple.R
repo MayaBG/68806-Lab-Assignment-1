6606b6fbf6ea9beddd4269c47b5bb3fb9e0391de
MayaBG <maya.bartov@mail.huji.ac.il>
2016-11-15 18:52:42 +0200

    added a second plot

---
+++ simple.R
@@ -0,0 +1,13 @@
+# simple file to read, analyze and plot
+
+# First we read a set of meteorological observations.
+# The file is constructed of a date, hour (24 observation per day, 3 days), temperature and humidity
+meteo = read.csv('meteo data.csv',header=T)
+
+
+# plot data
+plot(meteo$hour,meteo$temp,type="p",col="blue",main = "Temperture through the day",xlab = "Hour", ylab = "Temp [C]")
+
+# plotting a second graph
+plot(meteo$hour,meteo$hum,type = "p",col="red",main = "Humidity through the day",xlab = "Hour", ylab = "RH [%]")
+

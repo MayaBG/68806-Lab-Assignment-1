7c0a5b7298c4a05f7a915eeb57c9544b52bef085
MayaBG <maya.bartov@mail.huji.ac.il>
2016-11-16 10:10:29 +0200

    added the fuction to the package manually

---
+++ meteofunction/TD_calc.R
@@ -0,0 +1,15 @@
+#' Dew-point temparture calculator
+#' 
+#' This function calculates the Dew-point temperature from humidty and temperature data
+#' by using the eqution: 
+#' TD = 243.04*(LN(RH/100)+((17.625*T)/(243.04+T)))/(17.625-LN(RH/100)-((17.625*T)/(243.04+T)))
+#' 
+#' @param        temp    (deg C) air temperature
+#' @param        hum    (%) Relitive Humidity
+#' @return       TD     (deg C) dew-point temperature  
+#' 
+TD_calc = function(hum, temp){
+  
+  TD = 243.04*(log(hum/100)+((17.625*temp)/(243.04+temp)))/(17.625-log(hum/100)-((17.625*temp)/(243.04+temp)))   
+  return(TD)
+}
